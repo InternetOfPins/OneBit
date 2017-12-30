ac66aec22660069df78a99998bf4aa1288cdd497
Rui Azevedo <ruihfazevedo@gmail.com>
2017-12-30 18:45:08 -0100

    code review and test
    checked this down to disassembly to ensure inlineness and even when cross bounding

--- src/OneBit.h
+++ src/OneBit.h
@@ -10,98 +10,61 @@ namespace OneBit {
   typedef uint8_t Byte;
 
   //log base 2
-  constexpr inline unsigned char _log2(size_t n) {return (n?_log2(n>>1)+1:0);};
-  constexpr inline unsigned char log2(size_t n) {return n?_log2(n-1):0;};
+  template<Byte n>
+  constexpr inline unsigned char _log2() {return n?_log2<(n>>1)>()+1:0;}
+  template<Byte n>
+  constexpr inline unsigned char log2() {return n?_log2<(n>>1)>():0;}
 
   //bit operations within a bit field
   //operations get/set, on/off can cross bounds between units
   //bit fields size can even exceed unit size
-  template<typename Unit,typename Value=Unit>
-  struct Bits {
-    //bits per unit
-    constexpr static inline Byte bpu() {return sizeof(Unit)<<3;}
-    //address bits per unit
-    constexpr static inline Byte abpu() {return log2(bpu());}
-    //max representable value
-    constexpr static inline Value ones(Byte sz) {return (1<<sz)-1;}
-    //mask
-    constexpr static inline Value mask(Byte at=0,Byte sz=1) {return ones(sz)<<at;}
-    //unit index
-    constexpr static inline Byte idx(Byte at) {return at>>abpu();}
-    //bit address inside unit
-    constexpr static inline Byte pos(Byte at) {return at&(ones(abpu()));}
-    //get bit-field value
-    constexpr static inline Value get(Unit data[],Byte at,Byte sz) {
-      return (pos(at)+sz)>bpu()?//crossing bounds
-        get(data,at,bpu()-pos(at))|get(data,at+(bpu()-pos(at)),sz-(bpu()-pos(at)))<<(bpu()-pos(at)):
-        (data[idx(at)]>>pos(at))&(ones(sz));
-    }
+  #define CROSS_BOUNDS_FIELDS
+  #define DATA_TYPE uint8_t //Unit*
 
-    //set bit-field value
-    constexpr static inline void set(Unit data[],Byte at,Byte sz,Value value) {
-      Unit m=mask(pos(at),sz);
-      if ((pos(at)+sz)>bpu()) {//crossing bounds
-        set(data,at,bpu()-pos(at),value);
-        set(data,at+(bpu()-pos(at)),sz-(bpu()-pos(at)),value>>(bpu()-pos(at)));
-      } else data[idx(at)]=(data[idx(at)]&~m)|((value<<pos(at))&m);
+  template<typename Unit,DATA_TYPE data,uint8_t at, int8_t sz=1,typename Value=Unit>
+  struct Bits {
+    //some usefull functions for bits map math
+    //bits per unit
+    constexpr static inline uint8_t bpu() {return sizeof(Unit)<<3;}
+    //address bits per unit
+    constexpr static inline uint8_t abpu() {return log2<bpu()>();}
+    //max representable value
+    constexpr static inline Value ones(int8_t n=sz) {return (1<<n)-1;}
+    //mask
+    constexpr static inline Value mask() {return ones(sz)<<pos();}
+    //unit index
+    constexpr static inline uint8_t idx() {return at>>abpu();}
+    //bit address inside unit
+    constexpr static inline uint8_t pos() {return at&(ones(abpu()));}
+
+    // bit field operations get/set, on/off ---------------------------
+    template<typename T,T v>
+    class display;
+    enum {
+      psz=pos()+sz,
+      delta=bpu()-pos(),
+      crossing=(pos()+sz)>bpu()
+    };
+    static inline Value get() {
+      #pragma GCC diagnostic ignored "-Woverflow"
+        return ((pos()+sz)>bpu()) ?
+          (Bits<Unit,data,at,delta,Value>::get()|((Bits<Unit,data,at+delta,sz-delta,Value>::get())<<delta)) :
+          (reinterpret_cast<Unit*>(data)[idx()]>>pos())&ones();
+      #pragma GCC diagnostic pop
+    }
+    static inline void set(Value value) {
+      if (crossing) {
+        Bits<Unit,data,at,delta,Value>::set(value);
+        #pragma GCC diagnostic ignored "-Woverflow"
+        Bits<Unit,data,at+delta,sz-delta,Value>::set(value>>delta);
+        #pragma GCC diagnostic pop
+      } else
+        reinterpret_cast<Unit*>(data)[idx()]=(reinterpret_cast<Unit*>(data)[idx()]&~(mask()))|((value<<pos())&(mask()));
     }
     //turn bit-field on (all ones)
-    constexpr static inline void on(Unit data[],Byte at,Byte sz) {
-      set(data,at,sz,ones(sz));
-    }
+    static inline void on() {set(ones(sz));}
     //turn bit-field off (all zeros)
-    constexpr static inline void off(Unit data[],Byte at,Byte sz) {
-      set(data,at,sz,0);
-    }
-
-  };
-
-  // a sequence of bits on arbitrary data
-  template<typename Unit,Byte at, Byte sz=1,typename Value=Unit>
-  struct BitPart {
-    constexpr static inline Value get(Unit data[]) {
-      return Bits<Unit,Value>::get(data,at,sz);
-    }
-    constexpr static inline void on(Unit data[]) {
-      Bits<Unit,Value>::on(data,at);
-    }
-    constexpr static inline void off(Unit data[]) {
-      Bits<Unit,Value>::off(data,at);
-    }
-    constexpr static inline void set(Unit data[],Value value) {
-      Bits<Unit,Value>::set(data,at,sz,value);
-    }
-  };
-
-  // just an array of data with functions to work with bits
-  template<typename Unit,Unit data[],typename Value=Unit>
-  struct BitData {
-
-    constexpr static inline Value get(Byte at,Byte sz) {
-      return Bits<Unit,Value>::get(data,at,sz);
-    }
-
-    template<Byte at, Byte sz=1>
-    constexpr static inline Value get() {
-      return BitPart<Unit,at,sz,Value>::get(data);
-    }
-
-    constexpr static inline void set(Byte at,Byte sz,Value value) {
-      return Bits<Unit,Value>::set(data,at,sz,value);
-    }
-
-    template<Byte at, Byte sz=1>
-    constexpr static inline void set(Value value) {
-      return BitPart<Unit,at,sz,Value>::set(data,value);
-    }
-  };
-
-  template<typename Unit, Unit data[],Byte at, Byte sz=1,typename Value=Unit>
-  struct BitField:public BitPart<Unit,at,sz> {
-    constexpr static inline Value get() {return BitPart<Unit,at,sz,Value>::get(data);}
-    constexpr static inline void on() {BitPart<Unit,at,sz,Value>::on(data);}
-    constexpr static inline void off() {BitPart<Unit,at,sz,Value>::off(data);}
-    constexpr static inline void set(Value value) {BitPart<Unit,at,sz,Value>::set(data,value);}
+    static inline void off() {set(0);}
   };
 
 };
--- test/test.cpp
+++ test/test.cpp
@@ -1,105 +1,105 @@
 #include <iostream>
 #include <bitset>
-#include "src/OneBit.h"
+#include "../src/OneBit.h"
 
 using namespace std;
 using namespace OneBit;
 
-uint8_t data[]={0b11111111,0b11101011};
+uint8_t data[]={0b11101101,0b11101011};
 
 int main() {
   cout<<"Testing OneBit"<<endl;
   cout<<"test bpu - bits per unit ------------"<<endl;
-  cout<<"byte:"<<(int)Bits<uint8_t>::bpu()<<endl;
-  cout<<"word:"<<(int)Bits<uint16_t>::bpu()<<endl;
-  cout<<"int:"<<(int)Bits<int>::bpu()<<endl;
+  cout<<"byte:"<<(int)Bits<uint8_t,data,0,8>::bpu()<<endl;
+  cout<<"word:"<<(int)Bits<uint16_t,data,0,16>::bpu()<<endl;
+  cout<<"int:"<<(int)Bits<int,data,0,8>::bpu()<<endl;
 
   cout<<"address test bpu - bits per unit ------------"<<endl;
-  cout<<"byte:"<<(int)Bits<uint8_t>::abpu()<<endl;
-  cout<<"word:"<<(int)Bits<uint16_t>::abpu()<<endl;
-  cout<<"int:"<<(int)Bits<int>::abpu()<<endl;
+  cout<<"byte:"<<(int)Bits<uint8_t,data,0,8>::abpu()<<endl;
+  cout<<"word:"<<(int)Bits<uint16_t,data,0,16>::abpu()<<endl;
+  cout<<"int:"<<(int)Bits<int,data,0>::abpu()<<endl;
 
   cout<<"byte ones ------------"<<endl;
-  cout<<"1:"<<bitset<8>(Bits<uint8_t>::ones(1))<<endl;
-  cout<<"2:"<<bitset<8>(Bits<uint8_t>::ones(2))<<endl;
-  cout<<"3:"<<bitset<8>(Bits<uint8_t>::ones(3))<<endl;
-  cout<<"8:"<<bitset<8>(Bits<uint8_t>::ones(8))<<endl;
+  cout<<"1:"<<bitset<8>(Bits<uint8_t,data,0,1>::ones())<<endl;
+  cout<<"2:"<<bitset<8>(Bits<uint8_t,data,0,2>::ones())<<endl;
+  cout<<"3:"<<bitset<8>(Bits<uint8_t,data,5,3>::ones())<<endl;
+  cout<<"8:"<<bitset<8>(Bits<uint8_t,data,0,8>::ones())<<endl;
 
   cout<<"word ones ------------"<<endl;
-  cout<<"1:"<<bitset<16>(Bits<uint16_t>::ones(1))<<endl;
-  cout<<"2:"<<bitset<16>(Bits<uint16_t>::ones(2))<<endl;
-  cout<<"3:"<<bitset<16>(Bits<uint16_t>::ones(3))<<endl;
-  cout<<"16:"<<bitset<16>(Bits<uint16_t>::ones(16))<<endl;
+  cout<<"1:"<<bitset<16>(Bits<uint16_t,data,0,1>::ones())<<endl;
+  cout<<"2:"<<bitset<16>(Bits<uint16_t,data,0,2>::ones())<<endl;
+  cout<<"3:"<<bitset<16>(Bits<uint16_t,data,0,3>::ones())<<endl;
+  cout<<"16:"<<bitset<16>(Bits<uint16_t,data,0,16>::ones())<<endl;
 
   cout<<"mask ------------"<<endl;
-  cout<<"0,0:"<<bitset<8>(Bits<uint8_t>::mask(0,0))<<endl;
-  cout<<"0,1:"<<bitset<8>(Bits<uint8_t>::mask(0,1))<<endl;
-  cout<<"0,2:"<<bitset<8>(Bits<uint8_t>::mask(0,2))<<endl;
-  cout<<"2,3:"<<bitset<8>(Bits<uint8_t>::mask(2,3))<<endl;
+  cout<<"0,0:"<<bitset<8>(Bits<uint8_t,data,0,0>::mask())<<endl;
+  cout<<"0,1:"<<bitset<8>(Bits<uint8_t,data,0,1>::mask())<<endl;
+  cout<<"0,2:"<<bitset<8>(Bits<uint8_t,data,0,2>::mask())<<endl;
+  cout<<"2,3:"<<bitset<8>(Bits<uint8_t,data,2,3>::mask())<<endl;
 
   cout<<"idx ------------"<<endl;
-  cout<<"0:"<<(int)Bits<uint8_t>::idx(0)<<endl;
-  cout<<"7:"<<(int)Bits<uint8_t>::idx(7)<<endl;
-  cout<<"8:"<<(int)Bits<uint8_t>::idx(8)<<endl;
-  cout<<"12:"<<(int)Bits<uint8_t>::idx(12)<<endl;
+  cout<<"0:"<<(int)Bits<uint8_t,data,0,1>::idx()<<endl;
+  cout<<"7:"<<(int)Bits<uint8_t,data,7,1>::idx()<<endl;
+  cout<<"8:"<<(int)Bits<uint8_t,data,8,1>::idx()<<endl;
+  cout<<"12:"<<(int)Bits<uint8_t,data,12,1>::idx()<<endl;
 
   cout<<"pos ------------"<<endl;
-  cout<<"0:"<<(int)Bits<uint8_t>::pos(0)<<endl;
-  cout<<"7:"<<(int)Bits<uint8_t>::pos(7)<<endl;
-  cout<<"8:"<<(int)Bits<uint8_t>::pos(8)<<endl;
-  cout<<"12:"<<(int)Bits<uint8_t>::pos(12)<<endl;
+  cout<<"0:"<<(int)Bits<uint8_t,data,1,1>::pos()<<endl;
+  cout<<"7:"<<(int)Bits<uint8_t,data,7,1>::pos()<<endl;
+  cout<<"8:"<<(int)Bits<uint8_t,data,8,1>::pos()<<endl;
+  cout<<"12:"<<(int)Bits<uint8_t,data,12,1>::pos()<<endl;
 
   cout<<"get ------------"<<endl;
-  cout<<"0,3:"<<bitset<8>(Bits<uint8_t>::get(data,0,3))<<endl;
-  cout<<"2,4:"<<bitset<8>(Bits<uint8_t>::get(data,2,4))<<endl;
-  cout<<"8,4:"<<bitset<8>(Bits<uint8_t>::get(data,8,4))<<endl;
-  cout<<"9,4:"<<bitset<8>(Bits<uint8_t>::get(data,9,4))<<endl;
-  cout<<"10,4:"<<bitset<8>(Bits<uint8_t>::get(data,10,4))<<endl;
-  cout<<"11,4:"<<bitset<8>(Bits<uint8_t>::get(data,11,4))<<endl;
+  cout<<"0,3:"<<bitset<8>(Bits<uint8_t,data,0,3>::get())<<endl;
+  cout<<"2,4:"<<bitset<8>(Bits<uint8_t,data,2,4>::get())<<endl;
+  cout<<"8,4:"<<bitset<8>(Bits<uint8_t,data,8,4>::get())<<endl;
+  cout<<"9,4:"<<bitset<8>(Bits<uint8_t,data,9,4>::get())<<endl;
+  cout<<"10,4:"<<bitset<8>(Bits<uint8_t,data,10,4>::get())<<endl;
+  cout<<"11,4:"<<bitset<8>(Bits<uint8_t,data,11,4>::get())<<endl;
 
   cout<<"set ------------"<<endl;
-  Bits<uint8_t>::set(data,0,3,0);
+  Bits<uint8_t,data,0,3>::set(0);
   cout<<"0,3=0:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
-  Bits<uint8_t>::set(data,0,3,5);
+  Bits<uint8_t,data,0,3>::set(5);
   cout<<"0,3=5:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
-  Bits<uint8_t>::set(data,4,3,5);
+  Bits<uint8_t,data,4,3>::set(5);
   cout<<"4,3=5:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
-  Bits<uint8_t>::set(data,0,8,0);
+  Bits<uint8_t,data,0,8>::set(0);
   cout<<"0,8=0:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
-  Bits<uint8_t>::set(data,8,8,0xF0);
+  Bits<uint8_t,data,8,8>::set(0xF0);
   cout<<"8,8=0xF0:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
 
-  cout<<"on ------------"<<endl;
-  Bits<uint8_t>::on(data,1,6);
-  cout<<"1,6:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
-  Bits<uint8_t>::on(data,9,6);
-  cout<<"9,6:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
-
-  cout<<"off ------------"<<endl;
-  Bits<uint8_t>::off(data,2,4);
-  cout<<"2,4:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
-  Bits<uint8_t>::off(data,10,4);
-  cout<<"10,4:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
-
   // cout<<"on ------------"<<endl;
-  // Bits<uint8_t>::on(data,0,6);
-  // cout<<"0,6:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
-
-  cout<<"cross-bouds get ------------"<<endl;
-  cout<<"6,4:"<<bitset<8>(Bits<uint8_t>::get(data,6,4))<<endl;
-
-  cout<<"cross-bouds set ------------"<<endl;
-  Bits<uint8_t>::set(data,6,4,0xf);
-  cout<<"6,4=0xf:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
-
-  cout<<"BitPart ------------"<<endl;
-  typedef BitPart<uint8_t,0,16,uint16_t> M;
-  M::set(data,0b1111110000111111);
-  cout<<"set 0b1111110000111111:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
-  cout<<"get:"<<bitset<16>(M::get(data))<<endl;
-
-  cout<<"BitField ------------"<<endl;
-  typedef BitField<uint8_t,data,0,16,uint16_t> Md;
-  cout<<"get:"<<bitset<16>(Md::get())<<endl;
+  // Bits<uint8_t>::on(data,1,6);
+  // cout<<"1,6:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
+  // Bits<uint8_t>::on(data,9,6);
+  // cout<<"9,6:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
+  //
+  // cout<<"off ------------"<<endl;
+  // Bits<uint8_t>::off(data,2,4);
+  // cout<<"2,4:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
+  // Bits<uint8_t>::off(data,10,4);
+  // cout<<"10,4:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
+  //
+  // // cout<<"on ------------"<<endl;
+  // // Bits<uint8_t>::on(data,0,6);
+  // // cout<<"0,6:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
+  //
+  // cout<<"cross-bouds get ------------"<<endl;
+  // cout<<"6,4:"<<bitset<8>(Bits<uint8_t>::get(data,6,4))<<endl;
+  //
+  // cout<<"cross-bouds set ------------"<<endl;
+  // Bits<uint8_t>::set(data,6,4,0xf);
+  // cout<<"6,4=0xf:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
+  //
+  // cout<<"BitPart ------------"<<endl;
+  // typedef BitPart<uint8_t,0,16,uint16_t> M;
+  // M::set(data,0b1111110000111111);
+  // cout<<"set 0b1111110000111111:"<<bitset<8>(data[1])<<" "<<bitset<8>(data[0])<<endl;
+  // cout<<"get:"<<bitset<16>(M::get(data))<<endl;
+  //
+  // cout<<"BitField ------------"<<endl;
+  // typedef BitField<uint8_t,data,0,16,uint16_t> Md;
+  // cout<<"get:"<<bitset<16>(Md::get())<<endl;
 
 }

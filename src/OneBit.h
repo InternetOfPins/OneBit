6022a5d2cfbac7651af991b3bbba0269bc255264
Rui Azevedo <ruihfazevedo@gmail.com>
2017-12-31 20:47:59 -0100

    use both pointers to memory and to hardware

--- src/OneBit.h
+++ src/OneBit.h
@@ -19,9 +19,8 @@ namespace OneBit {
   //operations get/set, on/off can cross bounds between units
   //bit fields size can even exceed unit size
   #define CROSS_BOUNDS_FIELDS
-  #define DATA_TYPE uint8_t //Unit*
 
-  template<typename Unit,DATA_TYPE data,uint8_t at, int8_t sz=1,typename Value=Unit>
+  template<typename Unit,typename Type,Type data,uint8_t at, uint8_t sz=1,typename Value=Unit>
   struct Bits {
     //some usefull functions for bits map math
     //bits per unit
@@ -29,7 +28,7 @@ namespace OneBit {
     //address bits per unit
     constexpr static inline uint8_t abpu() {return log2<bpu()>();}
     //max representable value
-    constexpr static inline Value ones(int8_t n=sz) {return (1<<n)-1;}
+    constexpr static inline Value ones(uint8_t n=sz) {return (1<<n)-1;}
     //mask
     constexpr static inline Value mask() {return ones(sz)<<pos();}
     //unit index
@@ -38,26 +37,22 @@ namespace OneBit {
     constexpr static inline uint8_t pos() {return at&(ones(abpu()));}
 
     // bit field operations get/set, on/off ---------------------------
-    template<typename T,T v>
-    class display;
     enum {
       psz=pos()+sz,
       delta=bpu()-pos(),
       crossing=(pos()+sz)>bpu()
     };
+
     static inline Value get() {
-      #pragma GCC diagnostic ignored "-Woverflow"
-        return ((pos()+sz)>bpu()) ?
-          (Bits<Unit,data,at,delta,Value>::get()|((Bits<Unit,data,at+delta,sz-delta,Value>::get())<<delta)) :
-          (reinterpret_cast<Unit*>(data)[idx()]>>pos())&ones();
-      #pragma GCC diagnostic pop
+      return ((pos()+sz)>bpu()) ?
+        (Bits<Unit,Type,data,at,delta,Value>::get()|((Bits<Unit,Type,data,(uint8_t)(at+delta),(uint8_t)(sz-delta),Value>::get())<<delta)) :
+        (reinterpret_cast<Unit*>(data)[idx()]>>pos())&ones();
     }
+
     static inline void set(Value value) {
       if (crossing) {
-        Bits<Unit,data,at,delta,Value>::set(value);
-        #pragma GCC diagnostic ignored "-Woverflow"
-        Bits<Unit,data,at+delta,sz-delta,Value>::set(value>>delta);
-        #pragma GCC diagnostic pop
+        Bits<Unit,Type,data,at,delta,Value>::set(value);
+        Bits<Unit,Type,data,(uint8_t)(at+delta),(uint8_t)(sz-delta),Value>::set(value>>delta);
       } else
         reinterpret_cast<Unit*>(data)[idx()]=(reinterpret_cast<Unit*>(data)[idx()]&~(mask()))|((value<<pos())&(mask()));
     }
@@ -65,6 +60,8 @@ namespace OneBit {
     static inline void on() {set(ones(sz));}
     //turn bit-field off (all zeros)
     static inline void off() {set(0);}
+    static inline void tog() {set(~get());}
+
   };
 
 };
